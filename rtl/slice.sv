2f53f5339de7a2c71459598ef00aa1095e18aff2
Satvik Kasinadhuni <skasinad@gmail.com>
2026-03-21 12:45:17 -0400

    implemented sparsity genuinely hard af

--- rtl/slice.sv
+++ rtl/slice.sv
@@ -6,7 +6,7 @@ each slice needs to get one part of the 8-bit value coming from the register
 8 bits from accumulator value
 3 bits of opcode
 also 1 enable bit
-
+total 28 bits
 */
 
 
@@ -17,13 +17,40 @@ module slice#(
     input logic rst,
     input logic enable,
 
-    input logic[BYTE-1:0] elem1, 
-    input logic[BYTE-1:0] elem2,
+    input logic[BYTE-1:0] num1, 
+    input logic[BYTE-1:0] num2,
     input logic[BYTE-1:0] acum, 
     input logic[2:0] opcode,
     //value can be up to 16 bits because of the multplication instructions
     output logic[15:0] value  
 );
+    //only going to keep the opcodes up till VREUL b/c these are the only "mathematics" instructions
+    parameter logic[2:0] VADD = 3'b0;
+    parameter logic[2:0] VMUL = 3'b001;
+    parameter logic[2:0] VMAC = 3'b010; 
+    parameter logic[2:0] VDOT = 3'b011;
+    parameter logic[2:0] VRELU = 3'b100;
 
+    always_ff @(posedge clk) begin 
+        if(rst)
+            value <= 0;
+        else begin 
+            if(enable) begin 
+                case(opcode)
+                    VADD: value <= num1 + num2; 
+                    VMUL: value <= num1 * num2; 
+                    VMAC: value <= acum + (num1 * num2);
+                    VDOT: value <= num1 * num2; //just the same as VMUL b/c the adding happens in reduction
+                    VRELU: 
+                        if ($signed(num1) < 0) //completely forgot its unisgned by default had to use $signed() and then it started working
+                            value <= 0; 
+                        else 
+                            value <= num1; 
+                    default: value <= 0;
+                endcase 
+            end 
+          
+        end
+    end  
 
-endmodule;
+endmodule
---
+++ rtl/sparsity.sv
@@ -0,0 +1,49 @@
+/*
+This is my sparcity detection implementation.
+
+Need to check each of the operands from the slices if there are total 8 slices so then
+16 operands need to be checked totally
+
+8 of num1 8 of num2...
+
+maybe can define some sort of buffer 1s are all execute and then 0s for ignoring??
+
+inputs need to be the operands from the slice.. ahh maybe defining two operands as inputs each 8 bits long
+output will be the buffer?
+
+nvm need it for all 8
+opcode input b/c we dont need this for all the intructions
+
+sparsity only applies to VMUL VMAC and VDOT
+*/
+
+
+module sparsity (
+    input logic clk, 
+    input logic rst,
+    input logic[2:0] opcode, 
+    input logic[7:0] oper1[7:0],
+    input logic[7:0] oper2[7:0], 
+    output logic[7:0] buffer //realized after testing this only needs to be 8 b/c its just one slice
+);
+
+    always_ff @(posedge clk) begin 
+        if(rst) begin
+            buffer <= 8'b11111111; 
+        end else begin
+            if(opcode == 3'b001 || opcode == 3'b010 || opcode == 3'b011) begin
+                for(int i = 0; i < 8; i++) begin
+                    if(oper1[i] == 8'b0 || oper2[i] == 8'b0) begin
+                        buffer[i] <= 0; //that specific bit bcomes 0
+                    end else begin
+                        buffer[i] <= 1; 
+                    end   
+                end  
+                //need to also consider the other instructions
+            end else begin
+                buffer <= 8'b11111111;  
+
+            end
+        end
+    end   
+endmodule;
